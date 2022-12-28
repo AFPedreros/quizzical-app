010918ad598c9e5a30f0bb18cf518652f16a0d29
AFPedreros <felipe.pedreros94@gmail.com>
2022-12-27 21:38:34 -0500

    render wrong answers

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -13,13 +13,26 @@ interface QuestionInfo {
 export default function Question({ questionInfo }: Props) {
     const [question, setQuestion] = useState("");
     const [correctAnswer, setCorrectAnswer] = useState("");
+    const [wrongAnswer, setWrongAnswer] = useState([""]);
+
+    const renderWrongAnswers = wrongAnswer.map((answer, index) => {
+        return (
+            <div
+                key={index}
+                className=" mr-4 px-4 border-solid border-2 border-black rounded-lg"
+            >
+                {answer}
+            </div>
+        );
+    });
 
     useEffect(() => {
         setQuestion(questionInfo.question);
         setCorrectAnswer(questionInfo.correct_answer);
+        setWrongAnswer(questionInfo.incorrect_answers);
     }, []);
 
-    // console.log(questionInfo.incorrect_answers);
+    console.log(wrongAnswer);
 
     return (
         <div className=" my-4 py-2 border-b-2 border-slate-300 border-solid">
@@ -28,6 +41,7 @@ export default function Question({ questionInfo }: Props) {
                 <div className=" mr-4 px-4 border-solid border-2 border-black rounded-lg">
                     {correctAnswer}
                 </div>
+                {renderWrongAnswers}
             </div>
         </div>
     );
